5659e214d420e512d1d869a2665c186ca810b7d9
Adriane James Puzon <adrianejames27@gmail.com>
2026-04-26 01:20:55 +0800

    refactor(mobile): use derived state of delivery jobs to get the specific job detail

--- apps/swift-app/hooks/use-delivery-job.tsx
+++ apps/swift-app/hooks/use-delivery-job.tsx
@@ -1,30 +1,8 @@
-import DeliveryJobsService from "@/services/delivery-jobs.service";
-import { useDeliveryJobStore } from "@/store/delivery-job.store";
-
+import { useDeliveryJobsStore } from "@/store/delivery-jobs.store";
 
 const useDeliveryJob = (jobId: string) => {
-  const { job, loading, error, setJob, setLoading, setError } = useDeliveryJobStore();
-
-  async function fetchJob() {
-    setLoading(true);
-
-    try {
-      const result = await DeliveryJobsService.getJob(jobId);
-      setJob(result);
-    } catch (err) {
-      setError("Failed to load delivery job.");
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  return {
-    job, loading, error,
-    refetch: fetchJob
-  }
-
-  // return useDeliveryJobsStore((state) =>
-  //   state.jobs.find((job) => job.id === jobId))
+  return useDeliveryJobsStore((state) =>
+    state.jobs.find((job) => job.id === jobId))
 }
 
 export default useDeliveryJob;

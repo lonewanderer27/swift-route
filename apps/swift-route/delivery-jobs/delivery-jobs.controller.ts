8c73b455fd9d5a53d035229f94e1730d0d8909cf
Adriane James Puzon <adrianejames27@gmail.com>
2026-04-25 16:51:35 +0800

    feat(backend): implement static method for updating one delivery job

--- apps/swift-route/delivery-jobs/delivery-jobs.controller.ts
+++ apps/swift-route/delivery-jobs/delivery-jobs.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
 import { deliveryJobsStore } from "../src/stores";
 import { DeliveryJob } from "@swift-route/types";
 import { CreateDeliveryJobModel } from "./dto/create-delivery-job.dto";
+import { UpdateDeliveryJobModel } from "./dto/update-delivery-job.dto";
 
 @Controller("delivery-jobs")
 export class DeliveryJobsController {
@@ -10,15 +11,32 @@ export class DeliveryJobsController {
   findAll(): DeliveryJob[] {
     return deliveryJobsStore;
   }
+
   // TODO: GET /delivery-jobs/:id
   @Get(":id")
   findOne(@Param("id") id: string) {
     return deliveryJobsStore.find((record) => record.id === id);
   }
+
   // TODO: POST /delivery-jobs
   @Post()
   createOne(@Body() createDeliveryJobModel: CreateDeliveryJobModel) {
     return createDeliveryJobModel;
   }
+
+  // TODO: PUT /delivery-jobs/:id
+  @Put(":id")
+  updateOne(
+    @Param("id") id: string,
+    @Body() updateDeliveryJobModel: UpdateDeliveryJobModel,
+  ) {
+    const { id: _ignoredId, ...updatePayload } = updateDeliveryJobModel;
+
+    return {
+      id,
+      ...updatePayload,
+    };
+  }
+
   // TODO: PATCH /delivery-jobs/:id/status
 }

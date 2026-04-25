0bf5712c4473b32c81a53756c99a8feb501d3dbc
Adriane James Puzon <adrianejames27@gmail.com>
2026-04-26 00:23:36 +0800

    feat(mobile): create delivery job service

---
+++ apps/swift-app/services/delivery-job.service.ts
@@ -0,0 +1,31 @@
+import { DeliveryJob, DeliveryStatus } from "@swift-route/types";
+import api from "./api-client";
+
+class DeliveryJobService {
+  static async getCourierJobs(courierId: string, status: DeliveryStatus) {
+    const res = await api.get<DeliveryJob[]>(`delivery-jobs`, {
+      params: { courierId, status },
+    });
+    return res.data;
+  }
+  static async getJob(id: string) {
+    const res = await api.get<DeliveryJob>(`delivery-jobs/${id}`);
+    return res.data;
+  }
+  static async advanceStatus(id: string, status: DeliveryStatus) {
+    const res = await api.patch<DeliveryJob>(`delivery-jobs/${id}`, {
+      status,
+    });
+    return res.data;
+  }
+
+  // APIs not included in specification
+  static async getAllJobs(courierId: string, status: DeliveryStatus) {
+    const res = await api.get<DeliveryJob[]>(`delivery-jobs/`, {
+      params: { courierId, status },
+    });
+    return res.data;
+  }
+}
+
+export default DeliveryJobService;
